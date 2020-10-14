00df0dc0067246ea4b3b724e54e89d56cebb3be2
bdriguesdev <bdriguesdev@gmail.com>
2020-10-14 00:26:19 -0300

    add dot env

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/jinzhu/gorm v1.9.16
 	github.com/joaaomanooel/booksgo-api v0.0.0-20201010154803-44da4937e1ff
+	github.com/joho/godotenv v1.3.0
 )
--- main.go
+++ main.go
@@ -8,9 +8,16 @@ import (
 	models "recipehub-api/models"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
 	models.ConnectDatabase()
 
 	server := gin.Default()
