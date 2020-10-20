f2e1f427c0f19e91c464a2ccfd9503f010cb778c
João Manoel Neto <joaaomanooel@gmail.com>
2020-10-20 09:56:18 -0300

    create find user by id  method at user controller

--- controllers/users.go
+++ controllers/users.go
@@ -21,9 +21,20 @@ type UpdateUsersInput struct {
 	Password string `json:"password" binding:"required"`
 }
 
-func FindUsers(ginContext *gin.Context) {
+func FindUsers(context *gin.Context) {
 	var users []models.Users
 	models.DB.Find(&users)
 
-	ginContext.JSON(http.StatusOK, gin.H{"data": users})
+	context.JSON(http.StatusOK, gin.H{"data": users})
+}
+
+func FindUserById(context *gin.Context) {
+	var user models.Users
+
+	if err := models.DB.Where("id = ?", context.Param("id")).First(&user); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Record not found"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"data": user})
 }
