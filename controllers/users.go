859faeaf75031ab3dba76089094c60abfaa32b3a
João Manoel Neto <joaaomanooel@gmail.com>
2020-10-20 20:43:46 -0300

    create user controller

--- controllers/users.go
+++ controllers/users.go
@@ -38,3 +38,50 @@ func FindUserById(context *gin.Context) {
 
 	context.JSON(http.StatusOK, gin.H{"data": user})
 }
+
+func CreateUser(context *gin.Context) {
+	var input CreateUsersInput
+
+	if err := context.ShouldBindJSON(&input); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user := models.Users{
+		Name:     input.Name,
+		Username: input.Username,
+		Email:    input.Email,
+		Password: input.Password,
+	}
+
+	models.DB.Create(&user)
+	context.JSON(http.StatusOK, gin.H{"data": user})
+}
+
+func UpdateUser(context *gin.Context) {
+	var user models.Users
+	if err := models.DB.Where("id = ?", context.Param("id")).First(&user).Error; err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	var input UpdateUsersInput
+	if err := context.ShouldBindJSON(&input); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	models.DB.Model(&user).Updates(input)
+	context.JSON(http.StatusOK, gin.H{"data": user})
+}
+
+func DeleteUser(context *gin.Context) {
+	var user models.Users
+	if err := models.DB.Where("id = ?", context.Param("id")).First(&user).Error; err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	models.DB.Delete(&user)
+	context.JSON(http.StatusOK, gin.H{"data": true})
+}
